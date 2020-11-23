83bebcf25b7e7d3f2a68afa530f342ea4d8889d5
Kelsey Jackson <kelseyjackson@kelseys-mbp.attlocal.net>
2020-11-22 20:44:16 -0600

    adds server

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,21 +1,25 @@
-import { series, src, dest } from 'gulp'
+import { series, src, dest, watch } from 'gulp'
 import pug from 'gulp-pug'
 import sass from 'gulp-sass'
 import nodeSass from 'node-sass'
 import babel from 'gulp-babel'
+import bs from 'browser-sync'
 
+const browserSync = bs.create()
 sass.compiler = nodeSass
 
 function buildHtml() {
   return src('src/pug/pages/**/*.pug')
     .pipe(pug())
     .pipe(dest('build'))
+    .pipe(browserSync.stream())
 }
 
 function buildStyles() {
   return src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('build/css'))
+    .pipe(browserSync.stream())
 }
 
 function transpileJs() {
@@ -26,6 +30,23 @@ function transpileJs() {
       })
     )
     .pipe(dest('build/js'))
+    .pipe(browserSync.stream())
 }
 
-export default series(transpileJs, buildStyles, buildHtml)
+function server() {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    }
+  })
+  watch('src/pug/pages/**/*.pug', buildHtml)
+  watch('src/scss/**/*.scss', buildStyles)
+  watch('src/js/**/*.js', transpileJs)
+}
+
+export const build = series(
+  transpileJs,
+  buildStyles,
+  buildHtml
+)
+export default series(build, server)
